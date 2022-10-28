4f98c830a2733a4f1d37dd7e8503fe5d8d581d97
Alvin <alvin@adpost.com.tw>
2022-10-28 20:05:01 +0800

    init

---
+++ src/test/Test.php
@@ -0,0 +1,9 @@
+<?php
+namespace Alvin\ComposerV1;
+class Test
+{
+    public function test()
+    {
+        echo "This is my first composer package!";
+    }
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'alvin/composer-v1',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a7d349a3960b00f838594fccee35261e3eea95b6',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'alvin/composer-v1' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a7d349a3960b00f838594fccee35261e3eea95b6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
